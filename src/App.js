27c7ef827eb269bd4bf101c3c8248504e4a473f7
Amirul Faiz <amirulfaiz.work@gmail.com>
2022-03-08 07:57:02 +0800

    Create Home Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar/>
+      <Home/>
     </>
   );
 }
